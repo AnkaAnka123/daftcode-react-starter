27906ac773470589691f79405832a5ed5fd60781
AnkaAnka123 <anna.folwarska@gmail.com>
2018-04-24 12:01:35 +0200

    poprawiona

--- my-app/src/components/Counter/counter.js
+++ my-app/src/components/Counter/counter.js
@@ -1,39 +1,67 @@
 import React from 'react'
-import OnSuccess from './on-success'
-import Buttons from './buttons'
+
 import './counter.css'
 
-class Timer extends React.Component {
-  consturctor(props) {
-  
-    this.state = {
-      timingEvents: []
-    }
-    this.addTimerEvent = this.addTimerEvent.bind(this)
-    }
 
-    addTimerEvent(){
-    this.setState({
-      timingEvents:[
-        ...this.state.timingEvents,
-        new Date()
-      ]
-    })
+
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = { time: {}, seconds: 0 };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countUp = this.countUp.bind(this);
   }
-  
+
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      "h": hours,
+      "m": minutes,
+      "s": seconds
+    };
+    return obj;
+  }
+
+  componentDidMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer === 0) {
+      this.timer = setInterval(this.countUp, 1000);
+    }
+  }
+
+  countUp() {
+    
+    let seconds = this.state.seconds + 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+    }
+    
+    
+
   render() {
-  return (
-    <div className = 'container'>
-      <OnSuccess 
-       timingEvents = {this.state.timingEvents}/>
-      <Buttons 
-          handleClik={this.addTimerEvent}
-          timingEvents = {this.state.timingEvents}
-          />
-    </div>
-  )
-}
-}
+
+    return(
+      <div>
+        <button onClick={this.startTimer}>Start</button>
+        m: {this.state.time.m} s: {this.state.time.s}
+      </div>
+    );
+  }
+};
 
 
 export default Timer
